ec88d07a5a04be0c784efefe2a87f1d69ddbbd39
Shaxzodbek <shaxzodbek1703@gmail.com>
2024-05-29 05:46:59 -0700

    QR kod generate text

---
+++ app/src/main/java/com/example/texttopdf/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.texttopdf
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.texttopdf.databinding.ActivityMainBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnPdf.setOnClickListener {
+            val text = binding.edtInformation.text.toString()
+            if (text.isNotEmpty()) {
+                val qrCodeBitmap = generateQRCode(text)
+                binding.imageView.setImageBitmap(qrCodeBitmap)
+            }
+        }
+
+    }
+    private fun generateQRCode(text: String): Bitmap? {
+        val qrCodeWriter = QRCodeWriter()
+        return try {
+            val bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, 512, 512)
+            val width = bitMatrix.width
+            val height = bitMatrix.height
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) android.graphics.Color.BLACK else android.graphics.Color.WHITE)
+                }
+            }
+            bitmap
+        } catch (e: WriterException) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
